57789043c4e0ad6f16e40ae9c71128a023016075
jrohrssen <johnrohrssen@gmail.com>
2015-01-30 00:45:09 -0800

    latest

---
+++ plugin/Items/Button/StageButtonItem.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlPanelPlugin
+{
+  public class StageButtonItem : ButtonItem
+  {
+    public StageButtonItem(bool state = false)
+      : base(Constants.Panel.SwitchId.SWITCH_STAGE, state)
+    {
+
+    }
+
+    protected override void HandleSwitchStateMsg(Constants.Panel.SwitchId switchId, bool state)
+    {
+      if (Panel.stageArmed && Panel.canFireStage)
+      {
+        Panel.canFireStage = false;
+        Panel.CurrentVessel.ActiveNextStage();
+      }
+
+      SendState((int)Constants.Panel.StatusId.STAGE_ARMED, Panel.canFireStage);
+    }
+  }
+}
